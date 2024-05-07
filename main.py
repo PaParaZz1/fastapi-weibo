2a1bcfcd37e125fd9c7ecc2c14953072863b5dd0
niuyazhe <niuyazhe@sensetime.com>
2024-05-07 15:10:48 +0800

    polish

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from time import time
-from fastapi import FastAPI, Query, Request, __version__
+from fastapi import FastAPI, Form, Request, __version__
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import HTMLResponse
 import os
@@ -43,17 +43,12 @@ async def hello():
 
 
 @app.post('/check')
-#async def check(nonce: str = Query(None), timestamp: str = Query(None), echostr: str = Query(None), signature: str = Query(None)) -> str:
-async def check(request: Request) -> str:
+async def check(request: Request, nonce: str = Form(...), timestamp: str = Form(...), echostr: str = Form(...), signature: str = Form(...)) -> str:
+    # application/x-www-form-urlencoded
     logging.info(f"request: {request}")
     body = await request.body()
     logging.info(f"body: {body}")
     content_type = request.headers.get("Content-Type")
-    logging.info(f"content_type: {content_type} headers: {request.headers}")
-    nonce = await request.form("nonce")
-    timestamp = await request.form("timestamp")
-    echostr = await request.form("echostr")
-    signature = await request.form("signature")
     logging.info(f"nonce: {nonce}, timestamp: {timestamp}, echostr: {echostr}, signature: {signature}")
     cat_string = ''.join(sorted([timestamp, nonce, token]))
     if hashlib.sha1(cat_string.encode()).hexdigest() == signature:
